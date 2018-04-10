8897b3aed0abb65543378c3382c35bbdeb6288c4
olysec <joseolympio@gmail.com>
2018-04-10 11:40:37 -0300

    Aula 2 - Exercicio 1

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
-from flask import Flask
+import logging
+from flask import Flask, request
+from logging.handlers import RotatingFileHandler
+
 app = Flask(__name__)
+
+handler = RotatingFileHandler('/tmp/foo.log', maxBytes=10000, backupCount=1)
+handler.setLevel(logging.INFO)
+app.logger.addHandler(handler)
 
 @app.route("/")
 def hello():
+    app.logger.error(('The referrer was {}'.format(request.referrer)))
     return "Hello World!"
+
+
