f3ec4770f89ebc0f21e89693e98f2b81de26660a
olysec <joseolympio@gmail.com>
2018-04-10 16:13:58 -0300

    Aula 2.2

--- git.sh
+++ git.sh
@@ -3,7 +3,7 @@ git init .
 git add .
 git status
 git commit -m "Aula 2.2"
-git tag -a aula1 -m "Aula 2.2- olympio"
+git tag -a aula2.2 "Aula 2.2- olympio"
 git config --global user.email "joseolympio@gmail.com"
 git config --global user.name "olysec"
 git remote add origin https://github.com/olysec/Docker-course.git
